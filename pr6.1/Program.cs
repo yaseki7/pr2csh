d2161966c6069b82667ee28ddf3f9f6cb2f6ac32
yaseki7 <119028713+yaseki7@users.noreply.github.com>
2023-03-04 10:23:33 +0700

    Add files via upload

---
+++ pr6.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace _6
+{
+    
+    internal class Program
+    {
+        
+        static  void Main(string[] args)
+        {
+            Console.WriteLine("Введите город в котором хотите посмотреть градусы");
+            string gorod = Console.ReadLine();
+            string url = "https://api.openweathermap.org/data/2.5/weather?q="+gorod+"&units=metric&appid=d73a22636ce225fd4f1d095e2952190b";
+            
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            
+            
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream())) 
+            {
+                response = streamReader.ReadToEnd();
+                //Console.WriteLine(response);
+            }
+
+            WeatherRes weatherRes = JsonConvert.DeserializeObject<WeatherRes>(response);
+
+            Console.WriteLine("в {0} сейчас {1} градусов", weatherRes.Name,weatherRes.main.Temp);
+
+        }
+
+       
+    }
+}
---
+++ pr6.1/WeatherRes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    public class WeatherRes
+    {
+        public TempInfo main { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
