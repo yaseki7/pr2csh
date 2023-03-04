59d7e2efc5e2137b682664cbf1788a3595ba1045
yaseki7 <119028713+yaseki7@users.noreply.github.com>
2023-03-04 10:10:07 +0700

    Add files via upload

---
+++ p2_2/p2_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Train
+    {
+        public string pynkt, vremia;
+        public int nomer;
+
+        public void writeInConsoleInfo(string pynkt, string vremia, int nomer)
+        {
+            Console.WriteLine("Номер поезда: {0}\nВремя: {1}\nПункт: {2}", nomer, vremia, pynkt);
+        }
+    }
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Train poezd1 = new Train();
+            poezd1.pynkt = "Msk";
+            poezd1.nomer = 1;
+            poezd1.vremia = "22:00";
+
+            Train poezd2 = new Train();
+            poezd2.pynkt = "Msk";
+            poezd2.nomer = 2;
+            poezd2.vremia = "13:00";     
+            
+            Train poezd3 = new Train();
+            poezd3.pynkt = "Spb";
+            poezd3.nomer = 3;
+            poezd3.vremia = "8:00";   
+           
+            Console.WriteLine("Введите номер поезда от 1 до 3");
+            string chislo = Console.ReadLine();
+
+            switch (chislo)
+            {
+                case "1": 
+                    poezd1.writeInConsoleInfo(poezd1.pynkt, poezd1.vremia, poezd1.nomer);
+                    break;
+                case "2": 
+                    poezd2.writeInConsoleInfo(poezd2.pynkt, poezd2.vremia, poezd2.nomer);
+                    break;
+                case "3":
+                    poezd3.writeInConsoleInfo(poezd3.pynkt, poezd3.vremia, poezd3.nomer);
+                    break;
+            }
+        }
+    }
+}
---
+++ p2_3/p2_3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Storage
+    {
+        public int scetchik1, scetchik2;
+
+        public void writeInConsoleInfo(int scetchik1,int scetchik2)
+        {
+            Console.WriteLine("Первое число {0}\nВторое число {1}", scetchik1,scetchik2);
+        }
+
+
+    }
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Storage Schetov = new Storage();
+            Schetov.scetchik1 = 0;
+            Schetov.scetchik2 = 0;
+            bool b = true;
+            do
+            {
+                Console.WriteLine("Введите 1 для изменения первого числа, 2 для изменения второго числа, 3 для вывода суммы этих чисел, 4 для нахождения наибольшего числа, 5 для вывода просто двух чисел");
+                string a = Console.ReadLine();
+                switch (a)
+                {
+                    case "1":
+                       Console.Write("Пиши какое целое число хочешь ");
+                        //int newscetchik1 = Convert.ToInt32(Console.ReadLine());
+                        Schetov.scetchik1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case "2":
+                        Console.Write("Пиши какое целое число хочешь ");
+                        //int newscetchik2 = Convert.ToInt32(Console.ReadLine());
+                        Schetov.scetchik2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case "3":
+                        Console.Write($"Cумма этих чисел = {Schetov.scetchik1 + Schetov.scetchik2}\n");
+                        break;
+                    case "4":
+                        if (Schetov.scetchik1 > Schetov.scetchik2)
+                        {
+                            Console.Write("Первое число больше \n");
+                        }
+                        else Console.Write("Второе число больше \n");
+                        break;
+                    case "5":
+                        Schetov.writeInConsoleInfo(Schetov.scetchik1, Schetov.scetchik2);
+                        b = false;
+                        break;
+                }
+            } while (b);
+        }
+    }
+}
