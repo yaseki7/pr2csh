e33deef4c65651fd96a645bcd2c89549a6126105
yaseki7 <119028713+yaseki7@users.noreply.github.com>
2023-03-04 10:13:29 +0700

    Add files via upload

---
+++ pr2.4/pr2.4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Storage
+    {
+        public int scetchik1;
+
+        public void writeInConsoleInfo(int scetchik1)
+        {
+            Console.WriteLine(scetchik1);
+        }
+
+
+    }
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Storage Schetov = new Storage();
+            Schetov.scetchik1 = 0;
+            bool b = true;
+
+            do
+            {
+                Console.WriteLine("Введите 1 для прибавление единицы, 2 для уменьшения на единицу, 3 для вывода числа");
+                string a = Console.ReadLine();
+
+                switch (a)
+                {
+                    case "1":
+                        Schetov.scetchik1 = Schetov.scetchik1 + 1;
+                        break;
+                    case "2":
+                        Schetov.scetchik1 = Schetov.scetchik1 - 1;
+                        break;
+                    case "3":
+                        Console.Write($"Текущий счетчик = {Schetov.scetchik1}");
+                        b = false;
+                        break;
+                }
+            } while (b);
+        }
+    }
+}
---
+++ pr2.5/pr2.5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WaitForPendingFinalizersExample
+{
+    class Person 
+    {
+        public string name;
+        public int age;
+        public Person()
+        {
+            Console.WriteLine("Создание объекта Person");
+            name = "Tom";
+            age = 37;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+    
+    class MyWaitForPendingFinalizersClass
+    {
+  
+        const int numMfos = 1000;
+   
+        const int maxIterations = 100;
+
+        static void Main(string[] args)
+        {
+            string isxname = tom.name;
+            int isxage = tom.age;
+            Person tom = new Person();
+            Console.WriteLine("1 для вывода, 2 для отчистки, 3 для сброса к исходу");
+            string a = Console.ReadLine();
+      
+            switch(a){
+                case "1":
+                    tom.Print();
+                    break;
+                case "2":
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    for(int i = 0; i < maxIterations; i++)
+                    {
+                        Console.WriteLine("Doing some post-finalize work");
+                    }
+                    MyFinalizeObject mfo = null;
+                    for(int j = 0; j < numMfos; j++)
+                    {
+                        mfo = new MyFinalizeObject();
+                    }
+                    mfo = null;
+                    break;
+                case "3":
+                    isxname = tom.name;
+                    isxage = tom.age;
+                    break;
+            }
+        }
+    }
+
+    class MyFinalizeObject
+    {
+
+        private const int maxIterations = 1;
+
+        ~MyFinalizeObject()
+        {
+            Console.WriteLine("Finalizing a MyFinalizeObject");
+            
+     
+            for(int i = 0; i < maxIterations; i++)
+            {
+                GC.KeepAlive(i);
+            }
+        }
+    }
+}
